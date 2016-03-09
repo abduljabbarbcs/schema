0903a8beecf6674b2a543baa38f31ae341739da0
abduljabbarbcs <m.jabbar.bcs@gmail.com>
2016-03-09 13:24:23 +0500

    schema

--- visible_brain.sql
+++ visible_brain.sql
@@ -11,13 +11,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping database structure for visible_brain
-DROP DATABASE IF EXISTS `visible_brain`;
 CREATE DATABASE IF NOT EXISTS `visible_brain` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `visible_brain`;
 
 
 -- Dumping structure for table visible_brain.overlay_information
-DROP TABLE IF EXISTS `overlay_information`;
 CREATE TABLE IF NOT EXISTS `overlay_information` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `active` bit(1) NOT NULL,
@@ -35,11 +33,12 @@ CREATE TABLE IF NOT EXISTS `overlay_information` (
   CONSTRAINT `FK_lumx2skv3jwu9pg65gw1l6iku` FOREIGN KEY (`users`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table visible_brain.overlay_information: ~0 rows (approximately)
+/*!40000 ALTER TABLE `overlay_information` DISABLE KEYS */;
+/*!40000 ALTER TABLE `overlay_information` ENABLE KEYS */;
 
 
 -- Dumping structure for table visible_brain.overlay_points
-DROP TABLE IF EXISTS `overlay_points`;
 CREATE TABLE IF NOT EXISTS `overlay_points` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `x` double NOT NULL,
@@ -50,11 +49,12 @@ CREATE TABLE IF NOT EXISTS `overlay_points` (
   CONSTRAINT `FK_41h71e7ikqebg2bm36phmr4wr` FOREIGN KEY (`overlay_information_id`) REFERENCES `overlay_information` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table visible_brain.overlay_points: ~0 rows (approximately)
+/*!40000 ALTER TABLE `overlay_points` DISABLE KEYS */;
+/*!40000 ALTER TABLE `overlay_points` ENABLE KEYS */;
 
 
 -- Dumping structure for table visible_brain.slides
-DROP TABLE IF EXISTS `slides`;
 CREATE TABLE IF NOT EXISTS `slides` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `active` bit(1) NOT NULL,
@@ -66,13 +66,20 @@ CREATE TABLE IF NOT EXISTS `slides` (
   PRIMARY KEY (`id`),
   KEY `FK_dmfwhf6px2wlb0ug494m7xv0b` (`users`),
   CONSTRAINT `FK_dmfwhf6px2wlb0ug494m7xv0b` FOREIGN KEY (`users`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table visible_brain.slides: ~5 rows (approximately)
+/*!40000 ALTER TABLE `slides` DISABLE KEYS */;
+INSERT INTO `slides` (`id`, `active`, `name`, `description`, `type`, `users`, `slide_path`) VALUES
+	(1, b'1', 'brain', 'The brain is an organ that serves as the center of the nervous system in all vertebrate and most invertebrate animals. Only a few invertebrates such as sponges,ad.', 'male', 1, 'brain'),
+	(2, b'1', 'brain1', 'The brain is an organ that serves as the center of the nervous system in all vertebrate and most invertebrate animals. Only a few invertebrates such as sponges,ad.', 'male', 1, 'brain1'),
+	(3, b'1', 'brain2', 'The brain is an organ that serves as the center of the nervous system in all vertebrate and most invertebrate animals. Only a few invertebrates such as sponges,ad.', 'female', 1, 'brain'),
+	(4, b'1', 'brain3', 'The brain is an organ that serves as the center of the nervous system in all vertebrate and most invertebrate animals. Only a few invertebrates such as sponges,ad.', 'male', 1, 'brain1'),
+	(5, b'1', 'brain4', 'The brain is an organ that serves as the center of the nervous system in all vertebrate and most invertebrate animals. Only a few invertebrates such as sponges,ad.', 'male', 1, 'brain');
+/*!40000 ALTER TABLE `slides` ENABLE KEYS */;
 
 
 -- Dumping structure for table visible_brain.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `address` varchar(255) DEFAULT NULL,
@@ -88,9 +95,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_name` varchar(100) NOT NULL,
   `active` bit(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table visible_brain.users: ~1 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `address`, `company_name`, `email`, `first_name`, `last_login`, `last_name`, `password`, `profile_picture`, `role`, `token`, `user_name`, `active`) VALUES
+	(1, NULL, NULL, 'abc@example.com', NULL, '2016-03-09 12:11:58', NULL, '$2a$10$W11ub3rlcoPRhcereMAU/ODhoHrPyF4yvMViAYDUdDB33M92ZRnze', NULL, 'ROLE_ADMIN', '1', 'abc@example.com', b'1');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
